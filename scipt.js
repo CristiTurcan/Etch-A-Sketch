899c449da853b13e2760a37a153cc23ccc7a54e1
CristiTurcan <turcancristian02@gmail.com>
2023-04-25 22:15:18 +0300

    Implemented square coloring when hovering mouse over them

--- scipt.js
+++ scipt.js
@@ -1,11 +1,16 @@
 const container = document.querySelector('.container');
 
+function colorSquare(e) {
+    this.classList.add('pink');
+}
+
 function createGrid() {
     for(let i = 0; i < 256; i++) {
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseover', colorSquare);
 
-        container.appendChild(grid);
+        container.appendChild(square);
     }
 }
 
