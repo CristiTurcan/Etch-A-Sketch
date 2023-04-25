179beb1dbe9230e9ec4719605fcb0c0b15ec7b98
CristiTurcan <turcancristian02@gmail.com>
2023-04-26 00:10:13 +0300

    Added resizeable grid

--- scipt.js
+++ scipt.js
@@ -1,17 +1,33 @@
 const container = document.querySelector('.container');
+const containerSideSize = container.offsetHeight;
+
+const button = document.querySelector('button');
+button.addEventListener('click', createGrid);
 
 function colorSquare(e) {
-    this.classList.add('pink');
+    this.classList.add('black');
+}
+
+function enterGridSize() {
+    let size = prompt('Enter a grid size (0 - 100):');
+    const isInteger = /^(\d)+$/g; // check if number is integer
+    while(size > 100 || size <= 0 || !(isInteger.test(size))) {
+        size = prompt('Wrong value!\nEnter a grid size (0 - 100):');
+    }
+    return size;
 }
 
 function createGrid() {
-    for(let i = 0; i < 256; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.addEventListener('mouseover', colorSquare);
+    container.innerHTML = ''; // delete the previous grid
+    const size = enterGridSize();
+    const squareSideSize = containerSideSize / size - 2 + 'px';
 
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.addEventListener('mouseover', colorSquare);
+        square.classList.add('square');
+        square.style.height = squareSideSize;
+        square.style.width = squareSideSize;
         container.appendChild(square);
     }
 }
-
-createGrid();
