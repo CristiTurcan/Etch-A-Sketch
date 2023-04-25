8bd75472ffc22c2e1d4aabad3d1a71f67e5354bd
CristiTurcan <turcancristian02@gmail.com>
2023-04-25 21:52:56 +0300

    Added grid

--- scipt.js
+++ scipt.js
@@ -1,2 +1,12 @@
-const hello = document.querySelector('div');
-hello.textContent = 'Hello world';
+const container = document.querySelector('.container');
+
+function createGrid() {
+    for(let i = 0; i < 256; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+
+        container.appendChild(grid);
+    }
+}
+
+createGrid();
